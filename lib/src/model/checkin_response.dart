b74b4a0146efff078644913189f3b94258ae4b1e
Rahul Gupta <fc.rahulgupta@gmail.com>
2024-09-20 16:38:11 +0530

    Added Name in checkin response message.

--- lib/src/model/checkin_response.dart
+++ lib/src/model/checkin_response.dart
@@ -1,13 +1,16 @@
 class CheckInResponse {
   String? message;
   late int statusCode;
+  String? name;
 
-CheckInResponse({this.statusCode=200});
-  
+  CheckInResponse({this.statusCode = 200});
 
   CheckInResponse.fromJson(Map<String, dynamic> json) {
     if (json["message"] is String) {
       message = json["message"];
+    }
+    if (json["name"] is String) {
+      name = json["name"];
     }
   }
 }
